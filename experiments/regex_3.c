acda7f01104969bf71bb9bd7fd3ea80e1061f6ea
DeadDork <nimrodomer@gmail.com>
2013-01-05 05:53:53 -0800

    Prompts for arbitrary stings for regex & target.

--- experiments/regex_3.c
+++ experiments/regex_3.c
@@ -1,46 +1,56 @@
+////////////////////////////////////////////////////////////////////////////////
+// Comments
+
+// This plays further with the regex library.
+
+// Still only matches.
+
+// What makes it unique is the addition of the dynamic string custom library.
+
+////////////////////////////////////////////////////////////////////////////////
+// Libraries
+
 //#include <sys/types.h>
 #include <stdlib.h>
 #include <regex.h>
 #include <stdio.h>
+#include "../custom_libraries/dynamic_string/d_s.h"
 
+////////////////////////////////////////////////////////////////////////////////
 int main( void )
 {
 	regex_t regex;
-	char re[];
-	char s[];
-	char msgbuf[ 100 ];
-	int reti;
+	char* re;
+	char* s;
+	int re_err;
 
-/* Get regular expression sequence & test string. */
 	printf( "Enter a Regular Expression sequence: " );
-	scanf( "%s", re );
+	re = d_s( re );
 
 	printf( "Enter a test string to check the regex against: " );
-	scanf( "%s", s );
+	s = d_s( s );
 
-/* Compile regular expression */
-	reti = regcomp( &regex, re, 0 );
-	if( reti )
+	// Compile regular expression. 
+	if( ( re_err = regcomp( &regex, re, 0 ) ) )
 	{
 		fprintf( stderr, "Could not compile regex\n" );
 		exit( 1 );
 	}
 
-/* Execute regular expression */
-	reti = regexec( &regex, re, 0, NULL, 0 );
-	if( !reti )
-		puts("Match");
-	else if( reti == REG_NOMATCH )
-		puts("No match");
+	// Execute regular expression.
+	if( !( re_err = regexec( &regex, s, 0, NULL, 0 ) ) )
+		puts( "Match" );
+	else if( re_err == REG_NOMATCH )
+		puts( "No match" );
 	else
 	{
-		regerror( reti, &regex, s, sizeof( msgbuf ) );
-		fprintf( stderr, "Regex match failed: %s\n", msgbuf );
-		exit( 1 );
+		regerror( re_err, &regex, s, sizeof( s ) );
+		fprintf( stderr, "Regex match failed: %s\n", s );
+		exit( 2 );
 	}
 
-/* Free compiled regular expression if you want to use the regex_t again */
-regfree( &regex );
+	// Free compiled regular expression if you want to use the regex_t again.
+	regfree( &regex );
 
 	return 0;
 }
