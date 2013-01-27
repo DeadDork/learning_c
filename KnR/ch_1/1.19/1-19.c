25d6cf471e7e16ca1688e9951988a49d387e70a5
DeadDork <nimrodomer@gmail.com>
2013-01-26 17:38:24 -0800

    Init: solution to ex. 1-19

---
+++ KnR/ch_1/1.19/1-19.c
@@ -0,0 +1,75 @@
+////////////////////////////////////////////////////////////////////////////////
+// Comments
+
+// Solution to ex. 1-19.
+
+////////////////////////////////////////////////////////////////////////////////
+// Libraries
+
+#include <stdio.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// Symbolic Constants
+
+#define MAXLEN 100 // MAXimum LENgth of string
+
+////////////////////////////////////////////////////////////////////////////////
+// Function Prototypes
+
+void reverse( char string[] );
+/* Reverses the order of elements in a character array.
+ 
+   *string* = string that will have it's character order reversed (e.g. "abc" --> "cba"). */
+
+////////////////////////////////////////////////////////////////////////////////
+// Functions
+
+void reverse( char string[] )
+{
+	int e, sz; // Element, array SiZe
+	char temp[ MAXLEN ]; // TEMPorary array
+
+	for( sz = 0; string[ sz ] != '\0'; ++sz )
+	{
+		temp[ sz ] = string[ sz ];
+	}
+
+	for( e = 0; e < sz; ++e )
+	{
+		string[ e ] = temp[ sz - ( e + 1 ) ];
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+int main()
+{
+	int c, n; // Character, Number
+	char buf[ MAXLEN ]; // BUFfer
+
+	n = 0;
+	while( ( c = getchar() ) != EOF )
+	{
+		if( c != '\n' )
+		{
+			if( n < MAXLEN )
+			{
+				buf[ n++ ] = c;
+			}
+		}
+		else
+		{
+			buf[ --n ] = '\0';
+
+			if( n > 0 )
+			{
+				printf( "Original: [%s]\n", buf );
+				reverse( buf );
+				printf( "Reverse:  [%s]\n", buf );
+			}
+
+			n = 0;
+		}
+	}
+
+	return 0;
+}
