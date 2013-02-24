d845ba34c3d40ba3305f909fb51e4d6c379be82b
DeadDork <nimrodomer@gmail.com>
2013-02-23 21:00:39 -0800

    Major facelift of custom libraries.

--- custom_libraries/lib/sstring.c
+++ custom_libraries/lib/sstring.c
@@ -3,8 +3,10 @@
 
 // These functions perform specialized string functions I find myself using on a regular basis.
 
-// Be sure to free the memory for the dynamic character array!
-// Be sure to free the memory for substring!
+/* DON'T FORGET TO FREE:
+ * d_s
+ * substring
+ * sub */
 
 // There are serious rounding errors with str2num. Using the DOUBLE type solves them to a large degree, but this should be noted.
 
@@ -15,71 +17,35 @@
 #include <stdlib.h>
 #include <math.h>
 #include <regex.h>
-#include "rematch.h"
 #include "sstring.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 // Functions
 
-char* d_s( char *s ) // Dynamic String; String
+char * d_s( char * s )
 {
-	int c;
-	int sz = 0;
+	int c; // Character
+	int mult = 1; // MULTiple
+	int sz = 0; // SiZe
 
 	/* Dynamically allocates memory for a string input. */
-	s = malloc( sizeof( char ) );
+	s = malloc( sizeof( char ) * BUFSZ );
 	while( ( c = fgetc( stdin ) ) != '\n' && c != EOF )
 	{
-		s = realloc( s, sizeof( char ) * ++sz );
-		*( s + ( sz - 1 ) ) = c;
+		if( ++sz > ( mult * BUFSZ ) )
+		{
+			s = realloc( s, sizeof( char ) * BUFSZ * ++mult );
+		}
+		s[ sz - 1 ] = c;
 	}
-	*( s + sz ) = '\0';
+	s[ sz ] = '\0';
 
 	return s;
 }
 
-char* substring( char* source, int begin, int end )
+int slength( char * string )
 {
-	int st_ln, e;
-	char *t;
-	char *tt;
-
-	/* Sanity checks */
-	if( begin > end )
-	{
-		fprintf( stderr, "specprint:substring -- begin substring point is larger than the end point.\n" );
-		exit( 1 );
-	}
-
-	st_ln = slength( source );
-	if( begin > st_ln )
-	{
-		fprintf( stderr, "specprint:substring -- begin substring point is larger than the length of the string.\n" );
-		exit( 1 );
-	}
-	if( end > st_ln )
-	{
-		fprintf( stderr, "specprint:substring -- end substring point is larger than the length of the string.\n" );
-		exit( 1 );
-	}
-
-	/* Allocate enough memory for the substring */
-	t = malloc( sizeof( char ) * ( end - begin ) );
-	tt = t;
-
-	/* Copy the substring */
-	for( e = begin; e <= end; ++e )
-	{
-		*tt++ = source[ e ];
-	}
-	*tt = '\0';
-
-	return t;
-}
-
-int slength( char* string )
-{
-	int e;
+	int e; // Element
 
 	/* Get string length */
 	for( e = 0; string[ e ]; ++e );
@@ -87,18 +53,65 @@ int slength( char* string )
 	return e;
 }
 
-double str2num( char *numstring )
+char * substring( char * source, int begin, int end )
 {
-	char num_re[] = "^(-?)(0|[1-9][0-9]*)\\.?([0-9]*)$";
-	size_t nmatch = 4;
-	regmatch_t pmatch[ nmatch ];
-	char *s;
-	double value = 0.0;
-	int e;
-	int s_l;
+	int st_ln, e; // STring LeNgth, Element
+	char * t; // Temporary string
 
-	/* Checks string. If it's in the form of a number, it is converted into a number. */
-	if( rematch( num_re, numstring, nmatch, pmatch ) )
+	/* Sanity checks */
+	if( begin > end )
+	{
+		fprintf( stderr, "sstring, substring: begin point is after end point.\n" );
+		exit( 1 );
+	}
+
+	st_ln = slength( source );
+	if( begin > st_ln )
+	{
+		fprintf( stderr, "sstring, substring: begin point is not in string.\n" );
+		exit( 1 );
+	}
+	if( end > st_ln )
+	{
+		fprintf( stderr, "sstring, substring: end point is not in string.\n" );
+		exit( 1 );
+	}
+
+	/* Allocate enough memory for the substring */
+	t = malloc( sizeof( char ) * ( 1 + ( end - begin ) ) );
+
+	/* Copy the substring */
+	for( e = begin; e <= end; ++e )
+	{
+		t[ e - begin ] = source[ e ];
+	}
+	t[ e - begin ] = '\0';
+
+	return t;
+}
+
+double str2num( char * numstring )
+{
+	regex_t regex; // REGular EXPression (to be compiled)
+	char num_re[] = "^(-?)(0|[1-9][0-9]*)\\.?([0-9]*)$"; // NUMber Regular Expression string
+	size_t nmatch = 4; // Number of subexpression MATCHes
+	regmatch_t pmatch[ nmatch ]; // number of subexpression PMATCH structs
+	int re_err; // Regular Expression ERRor
+	char err_msg[ MAXBUF ]; // ERRor MeSsaGe
+	int e; // Element
+	char * s; // temporary String
+	int s_l; // String Length
+	double value = 0.0; // return VALUE
+
+	/* Compiles the regex */
+	if( ( re_err = regcomp( &regex, num_re, REG_EXTENDED ) ) )
+	{
+		fprintf( stderr, "Could not compile regex\n" );
+		exit( 1 );
+	}
+
+	/* Checks string for number-like properties. If it's in the form of a number, it is converted into a number. */
+	if( !( re_err = regexec( &regex, numstring, nmatch, pmatch, 0 ) ) )
 	{
 		/* Integer portion of the float */
 		s = substring( numstring, pmatch[ 2 ].rm_so, pmatch[ 2 ].rm_eo - 1 );
@@ -109,7 +122,7 @@ double str2num( char *numstring )
 		}
 
 		/* Decimal portion of the float */
-		if( ( pmatch[ 3 ].rm_eo ) - pmatch[ 3 ].rm_so > 0 )
+		if( pmatch[ 3 ].rm_eo - pmatch[ 3 ].rm_so > 0 )
 		{
 			s = substring( numstring, pmatch[ 3 ].rm_so, pmatch[ 3 ].rm_eo - 1 );
 			s_l = slength( s );
@@ -120,55 +133,74 @@ double str2num( char *numstring )
 		}
 
 		/* Signage of the float */
-		if( pmatch[ 1 ].rm_eo > 0 )
+		if( pmatch[ 1 ].rm_eo - pmatch[ 1 ].rm_so > 0 )
 		{
 			value = value * -1;
 		}
 	}
-	else
+	else if( re_err == REG_NOMATCH )
 	{
 		fprintf( stderr, "Source string does not have a good number format: %s\n", numstring );
 		exit( 1 );
 	}
+	else
+	{
+		regerror( re_err, &regex, err_msg, sizeof( char ) * MAXBUF );
+		fprintf( stderr, "Regex match failed: %s\n", err_msg );
+		exit( 1 ); 
+	}
+
+	free( s );
+	regfree( &regex );
 
 	return value;
 }
 
-char *res( char *source, char *substitute, int start, int stop )
+char * sub( char * source, char * substitute, int start, int stop )
 {
-	char *begin, *end, *tmp, *ttmp, *trgt;
+	char * begin, * end, * tmp, * ttmp, * trgt; // BEGIN string, END string, TeMPorary string, TeMPorary string 2, TaRgEt string
 
+	/* Unless the substitution is at the head of the source string, assigns to `begin` the head of the string */
 	if( start > 0 )
 	{
 		begin = substring( source, 0, start - 1);
 	}
 
+	/* Unless the substitution is at the tail of the source string, assigns to `end` the tail of the string */
 	if( stop < slength( source ) )
 	{
-		end = substring( source, stop, slength( source ) );
+		end = substring( source, stop + 1, slength( source ) );
 	}
 
-	trgt = realloc( source, sizeof( char ) * ( slength( begin ) + slength( substitute ) + slength( end ) ) );
+	trgt = malloc( sizeof( char ) * ( slength( begin ) + slength( substitute ) + slength( end ) ) );
+
+	/* Performs the substitution */
 	ttmp = trgt;
 
+	/* Head */
 	tmp = begin;
 	while( *tmp != '\0' )
 	{
 		*ttmp++ = *tmp++;
 	}
 
+	/* Body */
 	tmp = substitute;
 	while( *tmp != '\0' )
 	{
 		*ttmp++ = *tmp++;
 	}
 
+	/* Tail */
 	tmp = end;
 	while( *tmp != '\0' )
 	{
 		*ttmp++ = *tmp++;
 	}
 	*ttmp = '\0';
+
+	free( begin );
+	free( end );
 
 	return trgt;
 }
--- custom_libraries/lib/sstring.h
+++ custom_libraries/lib/sstring.h
@@ -6,57 +6,69 @@
 #define TRUE 1
 #define FALSE 2
 
+#define BUFSZ 1000 // BUFfer SiZe
+#define MAXBUF 1000 // MAXimum BUFfer size
+
 ////////////////////////////////////////////////////////////////////////////////
-char *d_s( char* string );
+char * d_s( char * string );
 /* Creates a character string of arbitary length.
 
-   *string* = the arbitrarily lengthed string.
+   `string` = The arbitrarily lengthed string.
 
-   Returns the pointer to the arbitrary lengthed string. */
+   Returns the pointer to the arbitrary lengthed string.
+   
+   N.B. There are limitations to the size of the dynamic character array: 1) physical memory 2) register size.
+   
+   N.B. This is not a safe function, such that it CAN completely eat up all of the system's memory if there is a memory leak.
+   
+   N.B. DON'T FORGET TO FREE UP THE MEMORY!!! */
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
-char *substring( char *source, int begin, int end );
-/* Copies a specified substring from a string.
- 
-   *source* = the source string that the substring will be extracted from.
-   *begin* = the start point of the substring in the string, in array notation (e.g. the first element is at position 0).
-   *end* = the end point of the substring in the string, in array notiation.
-
-   Returns the pointer to the target substring. */
-////////////////////////////////////////////////////////////////////////////////
-
-////////////////////////////////////////////////////////////////////////////////
-int slength( char *string );
+int slength( char * string );
 /* Measures the length of a string, in array notation.
  
-   *string* = the source string to be measured.
+   `string` = The source string to be measured.
 
    Returns the length of the string. */
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
-double str2num( char *numstring );
+char * substring( char * source, int begin, int end );
+/* Slices a substring from a string.
+ 
+   `source` = The source string that the substring will be extracted from.
+   `begin` = The start point of the substring in the string, in array notation (e.g. the first element is at position 0).
+   `end` = The end point of the substring in the string, in array notiation.
+
+   Returns the pointer to the target substring.
+
+   N.B. DON'T FORGET TO FREE UP THE MEMORY!!! */
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+double str2num( char * numstring );
 /* Converts a string that's a human-readable number into a computer readable number.
  
-   *numstring* = the source string that is to be converted into a real number.
+   `numstring` = The source string that is to be converted into a real number.
 
    Returns a float of the converted number. */
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
-char *res( char *source, char *substitute, int start, int stop );
-/* Substitutes the substring from *start* to *stop* in string *source* with string *substitute*.
+char * sub( char * source, char * substitute, int start, int stop );
+/* Substitutes the substring from `start` to `stop` in string `source` with string `substitute`.
    
-   *source* = the source string that will have a substring substituted within.
-   *substitute* = the string that will substitute the substring within source.
-   *start* = the array element in *source* corresponding to the first character of the substring to be substituted.
-   *stop* = the array element in *source* corresponding to the first character AFTER the last character of the substring to be substituted.
+   `source` = the source string that will have a substring substituted within.
+   `substitute` = the string that will substitute the substring within source.
+   `start` = the array element in `source` corresponding to the first character of the substring to be substituted, in array notation.
+   `stop` = the array element in `source` corresponding to the last character of the substring to be substituted, in array notation.
 
    Returns the pointer of the new string.
 
-   N.B. *stop* is equal to the last element in the substring + 1.
+   N.B. `stop` is equal to the last element in the substring + 1.
 
-   N.B. `res` comes from 'Regular Expression Substitution'. The idea is to use `rematch` to get *start* & *stop* on *source*. */
+   N.B. `res` comes from 'Regular Expression Substitution'. The idea is to use `rematch` to get `start` & `stop` on `source`. */
+////////////////////////////////////////////////////////////////////////////////
 
 #endif /* SSTRING_H */
