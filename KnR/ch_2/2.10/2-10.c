ab0732391dc967cb6028c0523a205371a66bd274
DeadDork <nimrodomer@gmail.com>
2013-04-12 13:38:04 -0700

    Added <ctype.h>

--- KnR/ch_2/2.10/2-10.c
+++ KnR/ch_2/2.10/2-10.c
@@ -5,10 +5,14 @@
 
 // Reads & prints input; if any of it is an alphabetical upper case, lowers it.
 
+// N.B. I've included a out "correct" alternative. However, as it's
+// edifying value is nil in this context, it's commented out.
+
 ////////////////////////////////////////////////////////////////////////////////
 // Libraries
 
 #include <stdio.h>
+//#include <ctype.h>
 
 ////////////////////////////////////////////////////////////////////////////////
 int main( void )
@@ -18,6 +22,7 @@ int main( void )
 	while( ( c = getchar() ) != EOF )
 	{
 		putchar( ( c >= 'A' && c <= 'Z' ) ? ( c + ( 'a' - 'A' ) ) : c );
+		//putchar( tolower( c ) );
 	}
 
 	return 0;
