f8747585dcb3e0728dafbbabd757c5efcec5ee32
DeadDork <nimrodomer@gmail.com>
2013-05-18 22:56:52 -0700

    Init: Explores return values from suppressed scanf format

---
+++ experiments/scanf_5.c
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////
+// Comments
+
+// Explores the return values of scanf when the * flag is used to suppress
+// assignment. Does it return a 0 or 1?
+
+// Conclusion: It returns 0.
+
+////////////////////////////////////////////////////////////////////////////////
+// Libraries
+
+#include <stdio.h>
+#include <stdlib.h>
+
+////////////////////////////////////////////////////////////////////////////////
+int main (void) {
+	int i;
+	char * s = malloc (500);
+	float  f,  ff,  fff,  ffff,  fffff;
+
+	printf ("Suppressed match:\n");
+	for (i = 1; i <= 5 && (fgets (s, 500, stdin) != NULL); ++i)
+		printf ("%d/5: Number of float matches = [%d]\n", i, sscanf (s, "%*f %*f %*f %*f %*f"));
+
+	printf ("\nAssigned match:\n");
+	for (i = 1; i <= 5 && (fgets (s, 500, stdin) != NULL); ++i)
+		printf ("%d/5: Number of float matches = [%d]\n", i, sscanf (s, "%f %f %f %f %f", &f, &ff, &fff, &ffff, &fffff));
+
+	free (s);
+
+	return 0;
+}
