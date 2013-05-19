8fef7e0560fbe2635228c463b78a6b997f6412d5
DeadDork <nimrodomer@gmail.com>
2013-05-18 22:33:36 -0700

    Init: explores autoincrementing a pointer

---
+++ experiments/autoincrement_2.c
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////////////
+// Comments
+
+// Explores autoincrementing a pointer while also dereferencing it.
+
+// I hope that this isn't an undefined behavior thingymajigy, but it certainly
+// appears as though `*(s++)` does exactly what you would think it does; namely
+// it dereferences `s`, executes the expression, then it autoincrements `s`.
+// That is, it doesn't autoincrement `s` then dereferences it (after all the
+// autoincrement is parenthesized). If it did the latter, then the 'G' in "God"
+// would not print.
+
+////////////////////////////////////////////////////////////////////////////////
+// Libraries
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+////////////////////////////////////////////////////////////////////////////////
+int main (void) {
+	char * s; // Generic string
+
+	s = malloc ((size_t) 50);
+	s = strcat (s, "God be with you, goodwife Wallace!\n");
+
+	while (*s != '\0')
+		putchar (*(s++));
+
+	return 0;
+}
