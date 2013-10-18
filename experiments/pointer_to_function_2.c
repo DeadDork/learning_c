5ef6fee8afef4aa5a5f169debbd31cc867dd5f6e
DeadDork <nimrodomer@gmail.com>
2013-10-17 23:38:59 -0700

    Init: shows function pointers are pretty much casts to a function

---
+++ experiments/pointer_to_function_2.c
@@ -0,0 +1,25 @@
+// Explores having a function argument point to another function.
+
+// More generic than `pointer_to_function_1`.
+
+// Conclusion: pointers to functions are pretty much casts to functions.
+
+#include <stdarg.h>
+#include <stdio.h>
+
+void print_expression(int (*)(int, int), int, int);
+int sum(int, int);
+
+int main(void) {
+	print_expression((int (*)(int, int))sum, 7, 9);
+
+	return 0;
+}
+
+void print_expression(int (*function)(int, int), int x, int y) {
+	printf("%d\n", (*function)(x, y));
+}
+
+int sum(int x, int y) {
+	return x + y;
+}
