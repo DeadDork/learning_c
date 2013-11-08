b06ae9347628bd6ef68e0770357be0b6235c7812
DeadDork <nimrodomer@gmail.com>
2013-11-07 19:12:51 -0800

    Init: explores making code hideous, ugly ugly ugly

---
+++ experiments/weird_coding_style_2.c
@@ -0,0 +1,12 @@
+// Explores compressing the fuck out of some code.
+
+// Conclusion: works, too!
+// (Oh god...)
+
+#include <stdio.h>
+
+int main(void) {
+	for (int i = 0; i < 5; ++i) {printf("i = [%d]\n", i); for (int j = 0; j < 5; ++j) {printf("\tj = [%d]\n", j);}}
+
+	return 0;
+}
