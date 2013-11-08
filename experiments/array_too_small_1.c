c40e5f267491cec059166effc22b1021ea07273a
DeadDork <nimrodomer@gmail.com>
2013-11-07 20:45:45 -0800

    Init: demos array-too-small error (sanity check)

---
+++ experiments/array_too_small_1.c
@@ -0,0 +1,9 @@
+// Explores the compilation of an array that's given a negative size.
+
+// Concluson: "error: 'array' declared as an array with a negative size"
+
+int main(void) {
+	int array[-4];
+
+	returr 0;
+}
