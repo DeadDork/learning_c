b929df58ccca4abab86ad02f760f3ebb8eca2332
DeadDork <nimrodomer@gmail.com>
2013-11-10 11:02:44 -0800

    Simplifies code significantly

--- experiments/factorial_1.c
+++ experiments/factorial_1.c
@@ -2,29 +2,18 @@
 
 // Main {{{
 #include <stdio.h>
+#include <stdlib.h>
 
 #define Abs(x) (((x) < 0) ? -(x) : (x))
 
-int get_number(int, char *[]);
 int factorial(int);
 
 int main(int argc, char *argv[]) {
-	printf("Factorial of the absolute value of %s = %d\n", argv[argc - 1], factorial(Abs(get_number(argc, argv))));
+	printf("Factorial of the absolute value of %s = %d\n", argv[argc - 1], factorial(Abs(atoi(argv[argc - 1]))));
 
 	return 0;
 }
 // Main }}}
-
-// Get the number from arguments {{{
-#include <stdlib.h>
-
-int get_number(int argc, char *argv[]) {
-	if (argc == 1)
-		return 0; // In case no input
-
-	return atoi(argv[1]);
-}
-// Get the number from arguments }}}
 
 int factorial(int number) {
 	return number * ((number > 1) ? factorial(number - 1) : 1);
